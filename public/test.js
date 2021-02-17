408f6baeb981cc65436edc4a5bb9b66030f3861b
DESKTOP-48B4POT\PLANIT <whdntjrsla12@naver.com>
2021-02-17 17:19:19 +0900

    finish

--- public/test.js
+++ public/test.js
@@ -1,18 +1,36 @@
-import { User } from "./classes/user.js";
-const me = {
-    name: 'woosek',
-    age: 27,
-    speak(text) {
-        console.log(text);
-    },
-    spend(amount) {
-        console.log('I spent', amount);
-        return amount;
+const addUID = (obj) => {
+    let uid = Math.floor(Math.random() * 100);
+    return Object.assign(Object.assign({}, obj), { uid });
+};
+let docOne = addUID({ name: 'wooseok', age: 27 });
+console.log(docOne.name);
+const docTwo = {
+    uid: 1,
+    data: {
+        name: 'woosek'
     }
 };
-const greetPerson = (person) => {
-    console.log('hello', person.name);
-};
-greetPerson(me);
-const Wooseok = new User('wooseok', 'https://localhost:5500/public', 12345678);
-console.log(Wooseok.introduceMyProfile());
+export {};
+// interface IsPerson {
+//     name :string;
+//     age :number;
+//     speak(a:string): void;
+//     spend(a:number): number;
+// }
+// const me: IsPerson = {
+//     name:'woosek',
+//     age :27,
+//     speak(text:string):void{
+//         console.log(text);
+//     },
+//     spend(amount:number):number{
+//         console.log('I spent', amount);
+//         return amount;
+//     }
+// };
+// const greetPerson = (person: IsPerson) =>{
+//     console.log('hello', person.name);
+// }
+// greetPerson(me);
+// const Wooseok = new User('wooseok','https://localhost:5500/public',12345678);
+// console.log(Wooseok.introduceMyProfile());
--- src/test.ts
+++ src/test.ts
@@ -1,30 +1,52 @@
 import {User} from "./classes/user.js";
 
-interface IsPerson {
-    name :string;
-    age :number;
-    speak(a:string): void;
-    spend(a:number): number;
+const addUID =<T>(obj: T) => {
+    let uid = Math.floor(Math.random()*100);
+    return {...obj, uid};
 }
 
-const me: IsPerson = {
-    name:'woosek',
-    age :27,
-    speak(text:string):void{
-        console.log(text);
-    },
-    spend(amount:number):number{
-        console.log('I spent', amount);
-        return amount;
+let docOne = addUID({name:'wooseok', age:27});
+
+console.log(docOne.name);
+
+interface Resource<T> {
+    uid : number;
+    data: T;
+}
+
+const docTwo: Resource<object> = {
+    uid : 1,
+    data: {
+        name : 'woosek'
     }
-};
-
-const greetPerson = (person: IsPerson) =>{
-    console.log('hello', person.name);
 }
 
-greetPerson(me);
+
+// interface IsPerson {
+//     name :string;
+//     age :number;
+//     speak(a:string): void;
+//     spend(a:number): number;
+// }
+
+// const me: IsPerson = {
+//     name:'woosek',
+//     age :27,
+//     speak(text:string):void{
+//         console.log(text);
+//     },
+//     spend(amount:number):number{
+//         console.log('I spent', amount);
+//         return amount;
+//     }
+// };
+
+// const greetPerson = (person: IsPerson) =>{
+//     console.log('hello', person.name);
+// }
+
+// greetPerson(me);
 
 
-const Wooseok = new User('wooseok','https://localhost:5500/public',12345678);
-console.log(Wooseok.introduceMyProfile());
+// const Wooseok = new User('wooseok','https://localhost:5500/public',12345678);
+// console.log(Wooseok.introduceMyProfile());
