9aeaa11bce2e22ee4cf744e6423778db2ce2fb87
DESKTOP-48B4POT\PLANIT <whdntjrsla12@naver.com>
2021-02-17 11:21:24 +0900

    finish function

--- public/test.js
+++ public/test.js
@@ -1,2 +1,20 @@
 "use strict";
-console.log('test');
+var add = function (a, b, c //c:나는 있을수도 있고 없을 수도 있어
+) {
+    if (c)
+        console.log(c);
+    else
+        console.log(a + b);
+};
+add(5, 10);
+add(10, 20, '나를 출력하거라');
+var add2 = function (a, b, c //c:나를 입력하지 않으면 100을 가질게
+) {
+    if (c === void 0) { c = 100; }
+    console.log(c);
+};
+add2(1, 2);
+var minus = function (a, b) {
+    return a - b;
+};
+console.log(minus(10, 2));
--- src/test.ts
+++ src/test.ts
@@ -1,1 +1,23 @@
-console.log('test');
+const add = (a: number, b:number, c?:number | string //c:나는 있을수도 있고 없을 수도 있어
+    ) => {
+    if(c)
+        console.log(c);
+    else 
+        console.log(a+b);
+}
+
+add(5,10);
+add(10,20,'나를 출력하거라');
+
+const add2 = (a: number, b:number, c:number | string = 100 //c:나를 입력하지 않으면 100을 가질게
+    ) => {
+        console.log(c);
+}
+
+add2(1,2);
+
+const minus = (a: number, b:number): number =>{
+    return a - b;
+}
+
+console.log(minus(10,2));
