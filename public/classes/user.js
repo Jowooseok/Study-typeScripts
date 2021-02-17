e31cde00b9e4cf5bc969accaecd723af12d37794
DESKTOP-48B4POT\PLANIT <whdntjrsla12@naver.com>
2021-02-17 16:44:04 +0900

    start interface

--- public/classes/user.js
+++ public/classes/user.js
@@ -1,15 +1,10 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.User = void 0;
-var User = /** @class */ (function () {
-    function User(name, photo, phone) {
+export class User {
+    constructor(name, photo, phone) {
         this.name = name;
         this.photo = photo;
         this.phone = phone;
     }
-    User.prototype.introduceMyProfile = function () {
-        return "My name " + this.name + " and phoneNumber is " + this.phone;
-    };
-    return User;
-}());
-exports.User = User;
+    introduceMyProfile() {
+        return `My name ${this.name} and phoneNumber is ${this.phone}`;
+    }
+}
--- public/test.js
+++ public/test.js
@@ -1,5 +1,18 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var user_js_1 = require("./classes/user.js");
-var Wooseok = new user_js_1.User('wooseok', 'https://localhost:5500/public', 12345678);
+import { User } from "./classes/user.js";
+const me = {
+    name: 'woosek',
+    age: 27,
+    speak(text) {
+        console.log(text);
+    },
+    spend(amount) {
+        console.log('I spent', amount);
+        return amount;
+    }
+};
+const greetPerson = (person) => {
+    console.log('hello', person.name);
+};
+greetPerson(me);
+const Wooseok = new User('wooseok', 'https://localhost:5500/public', 12345678);
 console.log(Wooseok.introduceMyProfile());
