9502c1522e1a14b30ebeb17d4a98a963208d196c
DESKTOP-48B4POT\PLANIT <whdntjrsla12@naver.com>
2021-02-17 14:21:14 +0900

    test class

--- src/test.ts
+++ src/test.ts
@@ -1,1 +1,18 @@
-console.log('test');
+class User{
+    name: string;
+    photo: string;
+    phone: number;
+
+    constructor(name:string, photo:string, phone:number){
+        this.name = name;
+        this.photo = photo;
+        this.phone = phone;
+    }
+
+    introduceMyProfile(){
+        return `My name ${this.name} and phoneNumber is ${this.phone}`;
+    }
+}
+
+const Wooseok = new User('wooseok','https://localhost:5500/public',12345678);
+console.log(Wooseok.introduceMyProfile());
